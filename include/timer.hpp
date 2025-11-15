9e911c652ac9115b7feb2a1ad4dae6c65d5437d2
Amin Gholizad <amin.gholizad@gmail.com>
2025-11-15 03:18:12 +0100

    update format

--- include/timer.hpp
+++ include/timer.hpp
@@ -4,35 +4,29 @@
 #include <chrono>
 #include <iostream>
 
-class Timer
-{
+class Timer {
 private:
-    using clock_t = std::chrono::steady_clock;
-    using milliseconds_t = std::chrono::duration<double, std::ratio<1, 1000>>;
-    using time_t = std::chrono::time_point<clock_t>;
+  using clock_t = std::chrono::steady_clock;
+  using milliseconds_t = std::chrono::duration<double, std::ratio<1, 1000>>;
+  using time_t = std::chrono::time_point<clock_t>;
 
-    std::chrono::time_point<clock_t> m_start{clock_t::now()};
+  std::chrono::time_point<clock_t> m_start{clock_t::now()};
 
 public:
-    Timer() : m_start{clock_t::now()}
-    {
-    }
+  Timer() : m_start{clock_t::now()} {}
 
-    void reset() { m_start = clock_t::now(); }
+  void reset() { m_start = clock_t::now(); }
 
-    double elapsed() const
-    {
-        return milliseconds_t(clock_t::now() - m_start).count();
-    }
-    bool is_time_remaining(double d)
-    {
-        return (elapsed() - d) > 1e-3;
-    }
-    void tic() { reset(); }
-    void toc()
-    {
-        auto elapsed_time{elapsed()};
-        std::cout << "It took " << elapsed_time << "ms to run\n";
-    }
+  [[nodiscard]] double elapsed() const {
+    return milliseconds_t(clock_t::now() - m_start).count();
+  }
+  [[nodiscard]] bool is_time_remaining(double duration) const {
+    return (elapsed() - duration) > 1e-3;
+  }
+  void tic() { reset(); }
+  void toc() const {
+    auto elapsed_time{elapsed()};
+    std::cout << "It took " << elapsed_time << "ms to run\n";
+  }
 };
 #endif // !TIMER_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
 #include "timer.hpp"
 
-int main()
-{
-    Timer t{};
-    t.tic();
-    for (int i{0}; i < 1000000; ++i)
-        int x = 10 * 10;
-    t.toc();
-    return 0;
+int main() {
+  Timer t{};
+  t.tic();
+  for (int i{0}; i < 1000000; ++i)
+    int x = 10 * 10;
+  t.toc();
+  return 0;
 }
