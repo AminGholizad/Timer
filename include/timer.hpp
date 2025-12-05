bc40b80f2e9ee8448252bab1f089836bc5766b3a
Amin Gholizad <amin.gholizad@gmail.com>
2025-12-06 00:41:49 +0100

    converted to header only lib with tests

--- include/timer.hpp
+++ include/timer.hpp
@@ -5,28 +5,28 @@
 #include <iostream>
 
 class Timer {
-private:
-  using clock_t = std::chrono::steady_clock;
-  using milliseconds_t = std::chrono::duration<double, std::ratio<1, 1000>>;
-  using time_t = std::chrono::time_point<clock_t>;
-
-  std::chrono::time_point<clock_t> m_start{clock_t::now()};
-
 public:
-  Timer() : m_start{clock_t::now()} {}
+  static constexpr size_t MILI = 1000;
+  using clock_t = std::chrono::steady_clock;
+  using milliseconds_t = std::chrono::duration<double, std::ratio<1, MILI>>;
+  static constexpr milliseconds_t MICRO = milliseconds_t(1e-3);
+  using time_t = std::chrono::time_point<clock_t>;
 
   void reset() { m_start = clock_t::now(); }
 
-  [[nodiscard]] double elapsed() const {
-    return milliseconds_t(clock_t::now() - m_start).count();
+  [[nodiscard]] milliseconds_t elapsed() const {
+    return clock_t::now() - m_start;
   }
-  [[nodiscard]] bool is_time_remaining(double duration) const {
-    return (elapsed() - duration) > 1e-3;
+  [[nodiscard]] bool is_time_remaining(milliseconds_t duration) const {
+    return (elapsed() - duration) > MICRO;
   }
   void tic() { reset(); }
   void toc() const {
     auto elapsed_time{elapsed()};
     std::cout << "It took " << elapsed_time << "ms to run\n";
   }
+
+private:
+  time_t m_start{clock_t::now()};
 };
 #endif // !TIMER_HPP
