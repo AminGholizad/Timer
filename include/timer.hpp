229d9538a7b3ee749b48d8f1e084ec1090954d21
Amin Gholizad <amin.gholizad@gmail.com>
2025-12-11 12:02:36 +0100

    added Timer as namespace

--- include/timer.hpp
+++ include/timer.hpp
@@ -3,30 +3,29 @@
 #pragma once
 #include <chrono>
 #include <iostream>
-
+namespace Timer {
+constexpr size_t THOUSAND = 1000ULL;
+using clock_t = std::chrono::steady_clock;
+using milliseconds_t = std::chrono::duration<double, std::ratio<1, THOUSAND>>;
+using time_t = std::chrono::time_point<clock_t>;
 class Timer {
-public:
-  static constexpr size_t MILI = 1000;
-  using clock_t = std::chrono::steady_clock;
-  using milliseconds_t = std::chrono::duration<double, std::ratio<1, MILI>>;
-  static constexpr milliseconds_t MICRO = milliseconds_t(1e-3);
-  using time_t = std::chrono::time_point<clock_t>;
+    static constexpr milliseconds_t MICRO = milliseconds_t(1e-3);
 
-  void reset() { m_start = clock_t::now(); }
+  public:
+    void reset() { m_start = clock_t::now(); }
 
-  [[nodiscard]] milliseconds_t elapsed() const {
-    return clock_t::now() - m_start;
-  }
-  [[nodiscard]] bool is_time_remaining(milliseconds_t duration) const {
-    return (elapsed() - duration) > MICRO;
-  }
-  void tic() { reset(); }
-  void toc() const {
-    auto elapsed_time{elapsed()};
-    std::cout << "It took " << elapsed_time << "ms to run\n";
-  }
+    [[nodiscard]] milliseconds_t elapsed() const { return clock_t::now() - m_start; }
+    [[nodiscard]] bool is_time_remaining(milliseconds_t duration) const {
+        return (elapsed() - duration) > MICRO;
+    }
+    void tic() { reset(); }
+    void toc() const {
+        auto elapsed_time{elapsed()};
+        std::cout << "It took " << elapsed_time << "ms to run\n";
+    }
 
-private:
-  time_t m_start{clock_t::now()};
+  private:
+    time_t m_start{clock_t::now()};
 };
+} // namespace Timer
 #endif // !TIMER_HPP
