f6f32fd5e34a2beadd6eed33a89489d478e620ac
Amin Gholizad <amin.gholizad@gmail.com>
2025-12-17 14:56:23 +0100

    added catch2 as test framework

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,24 +1,22 @@
 #include <Timer.hpp>
-#include <cassert>
+#include <catch2/catch_test_macros.hpp>
 
-int main() {
-    {
-        Timer::Timer timer1{};
-        Timer::Timer timer2{};
-        constexpr int repeteations = 10000;
-        constexpr Timer::milliseconds_t wait_time{1};
-        Timer::milliseconds_t count_time{};
-        timer1.tic();
-        for (int i{0}; i < repeteations; ++i) {
-            timer2.reset();
-            while (timer2.is_time_remaining(wait_time)) {
-                // wait...
-            }
-            count_time += timer2.elapsed();
+TEST_CASE("waitng == counting") {
+    Timer::Timer timer1{};
+    Timer::Timer timer2{};
+    constexpr int repeteations = 10000;
+    constexpr Timer::milliseconds_t wait_time{1};
+    Timer::milliseconds_t count_time{};
+    timer1.tic();
+    for (int i{0}; i < repeteations; ++i) {
+        timer2.reset();
+        while (timer2.is_time_remaining(wait_time)) {
+            // wait...
         }
-        auto duration = timer1.elapsed();
-        assert(duration > count_time);
-        assert(duration > (repeteations * (wait_time - Timer::MICRO)));
+        count_time += timer2.elapsed();
     }
-    return 0;
+    auto duration = timer1.elapsed();
+    REQUIRE(duration > count_time);
+    REQUIRE(duration < (repeteations * wait_time));
+    REQUIRE(duration > (repeteations * (wait_time - Timer::MICRO)));
 }
